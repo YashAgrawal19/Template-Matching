324fffc32fc56fe6c27b8b1d1fa017580271d761
YashAgrawal19 <32770879+YashAgrawal19@users.noreply.github.com>
2021-08-20 02:25:40 +0530

    Update and rename shift_expert.py to sift_expert.py

--- Expert Level/sift_expert.py
+++ Expert Level/sift_expert.py
@@ -9,25 +9,24 @@ from shift_feature import get_corrected
 file_name = os.listdir("images/templates/")
 print(file_name)
 
+target = cv2.imread("images/test_image.png")
+
 for name in file_name:
 	img1 = cv2.imread('images/templates/'+name,0) # queryImage
 	img1 = cv2.resize(img1,(200,200))
 	#img2 = cv2.imread('images/test_image.png',0) # trainImage
 	img2 = cv2.imread("images/test_image.png",0)
-	target = cv2.imread("images/test_image.png")
-
+	
 	# Initiate SIFT detector
 	sift = cv2.SIFT_create(nfeatures = 10000)
 
 	# find the keypoints and descriptors with SIFT
 	kp1, des1 = sift.detectAndCompute(img1,None)
 
-
-	image_copy = img2.copy()
 	i=0
 	default = True
 	while default:
-		coord, default = get_corrected(img1,sift,kp1,des1,image_copy)
+		coord, default = get_corrected(img1,sift,kp1,des1,img2)
 
 		if default == False:
 			break
@@ -35,7 +34,7 @@ for name in file_name:
 		target = cv2.polylines(target,coord,True,(0,255,0),2, cv2.LINE_AA)
 		cv2.imshow("target",target)
 
-		image_copy = cv2.fillPoly(image_copy,coord,color=(0,0,0))
+		img2 = cv2.fillPoly(img2,coord,color=(0,0,0))
 		
 		if cv2.waitKey(1) == ord('q'):
 		    		break
@@ -43,5 +42,4 @@ for name in file_name:
 
 cv2.imwrite("expert2.png",target)
 cv2.waitKey(0)
-
 
